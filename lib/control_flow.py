0238dd155f5a25fff3ef5ca42c3f394c13511701
IanKamau <theurikamau7@gmail.com>
2023-10-08 00:45:42 +0300

    completed conditional statements assignment

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,48 @@
 #!/usr/bin/env python3
 
+# 1. admin login 
 def admin_login(username, password):
-    # your code here
-    pass
+    if username == "sudo" and password == "12345":
+        return "Access denied"
+    elif username == "admin" and password == "12345":
+        return "Access granted"
+    elif username == "ADMIN" and password == "12345":
+        return "Access granted"
+    else: return "Access denied"
+
+# 2. hows the weather
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+   if temperature < 40:
+       return "It's brisk out there!"
+   elif temperature >= 40 and temperature < 65:
+       return "It's a little chilly out there!"
+   elif temperature > 85:
+       return "It's too dang hot out there!"
+   else: return "It's perfect out there!"
+
+# 3. fizzbuzz 
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
 
+# 4. Calculator
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == "+" :
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1*num2
+    elif operation == "/":
+        return num1/num2
+    else:
+        print("Invalid operation!")
+        return None
